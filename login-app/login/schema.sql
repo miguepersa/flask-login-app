88bdcd15d098721bdfade0faf115c6e704ff7a02
gchaurio <17-10126@usb.ve>
2023-02-10 22:38:38 -0400

    mamalo miguel

--- login-app/login/schema.sql
+++ login-app/login/schema.sql
@@ -4,7 +4,9 @@ DROP TABLE IF EXISTS post;
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
+  password TEXT NOT NULL,
+  role TEXT NOT NULL,
+  verified TEXT NOT NULL
 );
 
 CREATE TABLE post (
